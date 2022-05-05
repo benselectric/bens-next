18a1692327f67396df7f3864398ce2117da47a41
kiqecanijo <enry1375@gmail.com>
2022-05-05 10:41:18 -0500

    WIP contact images added

---
+++ pages/contacto.tsx
@@ -0,0 +1,16 @@
+import { Grid } from '@mui/material'
+
+import '@splidejs/splide/dist/css/themes/splide-default.min.css'
+
+const Contacto = () => (
+ <Grid container>
+  <Grid xs={12} sm={6}>
+   1
+  </Grid>
+  <Grid xs={12} sm={6}>
+   2
+  </Grid>
+ </Grid>
+)
+
+export default Contacto
